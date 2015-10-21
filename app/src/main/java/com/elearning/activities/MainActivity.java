e4c14842620b0663bbb535369f6515e010449c6e
ravisanker <ravisanker93@gmail.com>
2015-10-21 08:26:35 +0530

    Added basic folder structure to app

---
+++ app/src/main/java/com/elearning/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.elearning.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.elearning.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

bf8b8d713ae7d4cd7b76720b24fc59e97e7631b7
P <I306477@global.corp.sap>
2015-10-29 19:24:15 +0530

    Adding schema

--- models/user.js
+++ models/user.js
@@ -6,24 +6,41 @@ var UserSchema = mongoose.Schema({
     password: { type:String, required: true },
     fname: String,
     lname: String,
-    admin: Boolean,
     address: String,
     dob: Date,
-    createDate: Date,
-    updationDate: Date,
+    aadharCard :String,
+    enrollDate: Date,
     centre: String,
-    levels: [{completed:[String], remaining:[String], current:String}] 
+    type: [{'A','F','S'}] 
 });
 
 var LevelSchema = mongoose.Schema({
     levelName: { type: String, required: true, unique: true},
-    quizzes: { type: [String], required:true}
+    noOfHours: { type: [Number], required:true}
 });
 
+var CenterSchema = mongoose.Schema({
+    name: { type: String, unique: true},
+    faculty : String,
+    coordinator : String,
+    location:String
+});
+
+var StudentSchema = mongoose.Schema({
+    username :{ type: String, required: true, unique: true},
+    level: String,
+    noOfHoursCompleted : Number,
+    quizTaken : Boolean,
+    quizResult :  Boolean
+});
 
 var level =mongoose.model('Level', LevelSchema);
 var user = mongoose.model('User', UserSchema);
+var centre = mongoose.model('Centre', CenterSchema);
+var student = mongoose.model('Student', StudentSchema);
 module.exports = {
     User: user,
     Level: level
+    Centre : centre
+    Student : student
 };

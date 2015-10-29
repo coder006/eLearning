0562864074dd0bd8f4960fae0a2a1ec2c223a08e
Palanikumar <I306507@global.corp.sap>
2015-10-30 02:22:29 +0530

    Added title support SAPUI5

--- public/controller/Home.controller.js
+++ public/controller/Home.controller.js
@@ -5,15 +5,28 @@ sap.ui.define([
 
 	return BaseController.extend("sap.ui.demo.nav.controller.Home", {
 
-		onDisplayNotFound : function (oEvent) {
-			// display the "notFound" target without changing the hash
-			this.getRouter().getTargets().display("notFound", {
-				fromTarget : "home"
-			});
-		},
-
-		onNavToEmployees : function (oEvent){
-			this.getRouter().navTo("employeeList");
+		userLogin :  function () {
+			var username =  this.byId('userNameInput').getValue();
+			var password = this.byId('pwdInput').getValue();
+			var me = this;
+			if(!!username && !!password){
+				$.ajax({
+		            type: "POST",
+		            url: encodeURI("/login"),
+		            headers: {
+		                      "Content-Type": "application/json"
+		                     },
+		            data: JSON.stringify({"username": username, "password": password}),
+		            success: function( result ){
+						me.getRouter().navTo("employeeList", {user: username});
+		            },
+		            error: function(){
+		            	console.log("error");
+		            }
+		        });
+			} else {
+				this.byId('message').setText("Please enter proper credentials.");
+			}
 		}
 
 	});
--- public/controller/employee/EmployeeList.controller.js
+++ public/controller/employee/EmployeeList.controller.js
@@ -4,7 +4,35 @@ sap.ui.define([
 	"use strict";
 
 	return BaseController.extend("sap.ui.demo.nav.controller.employee.EmployeeList", {
+		onInit: function () {
+			var oRouter = this.getRouter();
+			this.userData = null;
+			oRouter.getRoute("employeeList").attachMatched(this._onRouteMatched, this);
 
+
+		},
+
+		_onRouteMatched: function(oEvent) {
+			var me = this;
+			var oArgs = oEvent.getParameter('arguments');
+			var username = oArgs.user;
+			$.ajax({
+				type: "GET",
+                url: encodeURI("/user/" + username),
+                headers: {
+                          "Content-Type": "application/json"
+                         },
+                success: function( userInfo){                 
+                    me.getView().byId('employeeListPage').setTitle("Welcome " + userInfo.fname +" !");
+                }
+		    });
+		    
+		},
+
+		setUserModel: function(userData) {
+			
+
+		}
 	});
 
 });

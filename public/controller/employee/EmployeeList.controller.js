b053f2fa13d6881ef8f800f2a8a6488027466e40
Ambar Mehrotra <mehrotraambar@gmail.com>
2015-10-30 09:28:53 +0530

    UI changes

--- public/controller/employee/EmployeeList.controller.js
+++ public/controller/employee/EmployeeList.controller.js
@@ -6,19 +6,19 @@ sap.ui.define([
 	return BaseController.extend("sap.ui.demo.nav.controller.employee.EmployeeList", {
 		onInit: function () {
 			var oRouter = this.getRouter();
-			this.userData = null;
+			this.username = null;
 			oRouter.getRoute("employeeList").attachMatched(this._onRouteMatched, this);
-
+             //this.renderSelectedTab(this.getView().byId('idIconTabBarNoIcons').getSelectedKey());
 
 		},
 
 		_onRouteMatched: function(oEvent) {
 			var me = this;
 			var oArgs = oEvent.getParameter('arguments');
-			var username = oArgs.user;
+			this.username = oArgs.user;
 			$.ajax({
 				type: "GET",
-                url: encodeURI("/user/" + username),
+                url: encodeURI("/user/" + me.username),
                 headers: {
                           "Content-Type": "application/json"
                          },
@@ -29,10 +29,91 @@ sap.ui.define([
 		    
 		},
 
-		setUserModel: function(userData) {
-			
+        onAfterRendering : function () {
+            this.renderSelectedTab(this.getView().byId('idIconTabBarNoIcons').getSelectedKey());
+        },
 
-		}
+		onListItemPress : function(oEvent) {
+            var me = this;
+            var studUsername;
+            var selectedfName = oEvent.getParameters().listItem.getTitle().split(' ')[0];
+            for(var i=0; i<this.studentList.length; i++) {
+               if(this.studentList[i].fname == selectedfName){
+                    studUsername = this.studentList[i].username;
+               }
+            }
+
+            $.ajax({
+                type: "GET",
+                url: encodeURI("/student/" + studUsername),
+                headers: {
+                          "Content-Type": "application/json"
+                         },
+                success: function( studInfo){                 
+                   me.renderStudentInfoDetail(studInfo);
+                }
+            });
+
+            /*var sToPageId = oEvent.getParameter("listItem").getCustomData()[0].getValue();
+
+            this.getSplitAppObj().toDetail(this.createId(sToPageId));*/
+        },
+
+        renderStudentInfoDetail: function(studInfo){
+            console.log(studInfo);
+        },  
+
+        insertStudentListItems: function(studentInfo) {
+            this.studentList = studentInfo;
+            var items = this.getView().byId('studList');
+            items.removeAllItems();
+            for(var i=0; i<studentInfo.length; i++) {
+                var stud = studentInfo[i];
+                var listItem = new sap.m.StandardListItem({
+                    title: stud.fname + ' ' + stud.lname,
+                    description: stud.address,
+                    icon: "./images/" + stud.username + ".jpg",
+                });
+                items.addItem(listItem);
+            }
+
+        },
+
+        handleTabSelection: function(oEvent) {
+            var me = this;
+            var sKey = oEvent.getParameters().key;
+            this.renderSelectedTab(sKey);
+        },
+
+        renderSelectedTab: function(key) {
+            var me = this;
+            if(key == 'stud') {
+                console.log('stud found');
+                $.ajax({
+                    type: "GET",
+                    url: encodeURI("/api/studentsByFaculty"),
+                    data: {
+                        'username': me.username
+                    },
+                    headers: {
+                              "Content-Type": "application/json"
+                             },
+                    success: function( studList ){                 
+                        // me.getView().byId('employeeListPage').setTitle("Welcome " + userInfo.fname +" !");
+                        console.log(studList);
+                        me.insertStudentListItems(studList); 
+                    },
+                    error: function(err) {
+                        console.log(err);
+                    }
+                });
+            }
+            else if(key == 'repo') {
+                console.log('this repo is hit');
+            }
+
+        }
+
 	});
 
 });
